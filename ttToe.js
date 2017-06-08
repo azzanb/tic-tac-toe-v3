02ad025a16570618ae121e40f810db3a2949ede9
Azzan Braxton <azzanbraxton@gmail.com>
2017-06-08 16:39:22 +0800

    Update

--- ttToe.js
+++ ttToe.js
@@ -20,6 +20,7 @@ function startPage(){
 	$('body').prepend(div);
 }
 
+
 var wins = [
 			[0,1,2], 
 			[3,4,5], 
@@ -30,11 +31,12 @@ var wins = [
 			[0,4,8], 
 			[2,4,6] 
 ];
-
+var clone = wins.map(function(arr){
+	return arr.slice();
+});
 //Everytime the button is clicked a new game begins
 function startGame(){ 
 	$('.button').click(function(){
-		$('.box').on('click');
 		$('#player1').addClass('active');
 		$('#player2').removeClass('active');
 		$('.screen').removeClass('screen-win screen-win-one screen-win-two screen-win-tie');
@@ -42,7 +44,8 @@ function startGame(){
 		$('.box').removeAttr('style');
 		$('.screen').hide();
 		$('#board').show();
-
+		console.log(clone);
+		clone;
 	});
 }
 
@@ -72,21 +75,18 @@ function winner(){
 // }
 
 
-var clickOn = false;
 function clicking(){
-	if(!clickOn) return;
-	$('.players').toggleClass("active");
 	
-	if($('#player1').hasClass("active")){
-		$(this).addClass("box-filled-2"); //it works, but it's is backwards	
+	if($('#player1').hasClass("active") && !$(this).hasClass("box-filled-1") && !$(this).hasClass("box-filled-2")){
+		$(this).addClass("box-filled-1"); //it works, but it's is backwards	
+		$('.players').toggleClass("active");
 	}
-	if($('#player2').hasClass("active")){
-		$(this).addClass("box-filled-1"); //it works, but it's is backwards
+	if($('#player2').hasClass("active") && !$(this).hasClass("box-filled-1") && !$(this).hasClass("box-filled-2")){
+		$(this).addClass("box-filled-2"); //it works, but it's is backwards
+		$('.players').toggleClass("active");
 	}
 }
-
-$('.box').on('click', clicking);
-clickOn = true;
+$('.box').click(clicking);
 
 
 
@@ -114,10 +114,10 @@ function Game(/*player1Num, box1Num, playr2Num, box2Num*/){
 
 	this.hover = function(player1Num, player2Num){
 		$('.box').hover(function(){ 
-			if($('#player' + player1Num).hasClass("active") && !$(this).hasClass('box-filled-2')){
+			if($('#player1').hasClass("active") && !$(this).hasClass('box-filled-2')){
 				$(this).css({backgroundImage: "url('img/o.svg')"});
 			}
-			else if($('#player' + player2Num).hasClass("active") && !$(this).hasClass('box-filled-1') ){
+			else if($('#player2').hasClass("active") && !$(this).hasClass('box-filled-1') ){
 				$(this).css({backgroundImage: "url('img/x.svg')"})
 			}
 		},
@@ -128,38 +128,35 @@ function Game(/*player1Num, box1Num, playr2Num, box2Num*/){
 
 	//Loops through array of possible win solutions to determine a winner
 	this.win = function(playerNum, winNum){
-		var clone = []
-		clone = wins.map(function(arr){
-				return arr.slice();
-		});
-		$('.box').on("click", function() {
-			console.log(clone);
-			for(var i = 0; i < clone.length; i++){
-				var winsIndex = clone[i];
-				var findIndex = $.inArray($(this).index(), winsIndex);
-				
-				if( findIndex > -1 && $(this).hasClass("box-filled-" + playerNum) ){
-				 	winsIndex.splice(findIndex, 1);
-					if(winsIndex.length === 0){ //if one sub array in wins has no more values
-						$(div).addClass("screen-win screen-win-" + winNum);
-						$(p).text("Winner");
-						winner(); 
-						//clone = [];
-						clone = wins.map(function(arr){
-							return arr.slice();
-						});
+		// clone = wins.map(function(arr){
+		// 		return arr.slice();
+		// });
 
-					}
+		$('.box').click(function() {
+			for(var i = 0; i < clone.length; i++){
+				var cloneIndex = clone[i];
+				var findIndex = $.inArray($(this).index(), cloneIndex);
+
+				if( findIndex > -1 && $('#player1.active')){
+				 	cloneIndex.splice(findIndex, 1);		
+				}			
+				if(cloneIndex.length === 0){ 
+					console.log(clone);
+					clone = wins.map(function(arr){
+						return arr.slice();
+					});
+					$(div).addClass("screen-win screen-win-" + winNum);
+					$(p).text("Winner");
+					winner(); 
 				}
 			}
 		});
+		return false;
 	}
 }
 
 var player1 = new Game();
 var player2 = new Game();
-//player1.click();
-//player2.click("2", "1", "1", "2");
 player1.hover("1", "2");
 player1.win("1","one");
 //player2.win("2", "two");
